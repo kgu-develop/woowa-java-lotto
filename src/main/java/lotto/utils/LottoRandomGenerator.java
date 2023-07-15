c96e0e671fe80844fe67314c7850fe06f4ac6dd3
SeoJiWon <sjiwon4491@gmail.com>
2023-07-15 21:07:16 +0900

    fix: UserLottos generate()시 unmodifiable 예외 처리를 위한 wrapping

--- src/main/java/lotto/utils/LottoRandomGenerator.java
+++ src/main/java/lotto/utils/LottoRandomGenerator.java
@@ -2,16 +2,19 @@ package lotto.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static lotto.utils.LottoConstants.*;
 
 public class LottoRandomGenerator {
     public static List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(
-                MIN_VALUE,
-                MAX_VALUE,
-                LOTTO_SIZE
+        return new ArrayList<>(
+                Randoms.pickUniqueNumbersInRange(
+                        MIN_VALUE,
+                        MAX_VALUE,
+                        LOTTO_SIZE
+                )
         );
     }
 }
