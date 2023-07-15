5dae29f2f9ac2b98de174ef2a412c38cf764d888
SeoJiWon <sjiwon4491@gmail.com>
2023-07-15 15:46:16 +0900

    feat: 로또 당첨 번호 & 보너스 번호를 기록하는 LottoWinningMachine 구현

---
+++ src/main/java/lotto/model/LottoWinningMachine.java
@@ -0,0 +1,43 @@
+package lotto.model;
+
+import java.util.List;
+
+import static lotto.utils.ExceptionConstants.LottoMachineException.BONUS_NUMBER_MUST_BE_UNIQUE;
+
+public class LottoWinningMachine {
+    private final Lotto winningLottery;
+    private final int bonusNumber;
+
+    private LottoWinningMachine(
+            final List<Integer> winningNumbers,
+            final int bonusNumber
+    ) {
+        validateBonusNumberIsDuplicate(winningNumbers, bonusNumber);
+        this.winningLottery = new Lotto(winningNumbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static LottoWinningMachine drawWinningLottery(
+            final List<Integer> winningNumbers,
+            final int bonusNumber
+    ) {
+        return new LottoWinningMachine(winningNumbers, bonusNumber);
+    }
+
+    private void validateBonusNumberIsDuplicate(
+            final List<Integer> winningNumbers,
+            final int bonusNumber
+    ) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_MUST_BE_UNIQUE.message);
+        }
+    }
+
+    public List<Integer> getWinningLotteryNumbers() {
+        return winningLottery.getNumbers();
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
